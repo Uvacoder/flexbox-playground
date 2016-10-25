74b2e22dde41f333ee18c22b9ae705662ae87e22
Mike McBride <mike.mcbride@wwt.com>
2016-10-25 00:39:25 -0500

    be more DRY

--- src/FlexItem.js
+++ src/FlexItem.js
@@ -45,29 +45,33 @@ export default class FlexItem extends Component {
   }
   
   render() {
+    
     const sx = Object.assign({}, this.state)
+    const inputClassNames = "dib courier white bg-transparent w4 outline-0 input-reset br0 ba bw1 bt-0 br-0 bl-0 b--white"
+    const labelClassNames = "w4 pr1 courier white f6 tr dib"
+    
     return (
       <div className="pa1" style={sx}>
         <div className="br3 bg-blue pv3 pr2 pl0">
           <div className="mb3">
-            <label className="w4 pr1 courier white f6 tr dib">order:</label>
-            <input type="number" placeholder="order" className="dib courier w4 white bg-transparent outline-0 input-reset ba bw1 bt-0 br-0 bl-0 b--white" value={this.state.order} onChange={this.handleOrderChange} />
+            <label className={labelClassNames}>order:</label>
+            <input type="number" placeholder="order" className={inputClassNames} value={this.state.order} onChange={this.handleOrderChange} />
           </div>
           <div className="mb3">
-            <label className="w4 pr1 courier white f6 tr dib">flexGrow:</label>
-            <input type="text" placeholder="flex-grow" className="dib courier w4 white bg-transparent outline-0 input-reset ba bw1 bt-0 br-0 bl-0 b--white" value={this.state.flexGrow} onChange={this.handleGrowChange} />
+            <label className={labelClassNames}>flexGrow:</label>
+            <input type="text" placeholder="flex-grow" className={inputClassNames} value={this.state.flexGrow} onChange={this.handleGrowChange} />
           </div>
           <div className="mb3">
-            <label className="w4 pr1 courier white f6 tr dib">flexShrink:</label>
-            <input type="text" placeholder="flex-shrink" className="dib courier w4 white bg-transparent outline-0 input-reset ba bw1 bt-0 br-0 bl-0 b--white" value={this.state.flexShrink} onChange={this.handleShrinkChange} />
+            <label className={labelClassNames}>flexShrink:</label>
+            <input type="text" placeholder="flex-shrink" className={inputClassNames} value={this.state.flexShrink} onChange={this.handleShrinkChange} />
           </div>
           <div className="mb3">
-            <label className="w4 pr1 courier white f6 tr dib">flexBasis:</label>
-            <input type="text" placeholder="flex-basis" className="dib courier w4 white bg-transparent outline-0 input-reset ba bw1 bt-0 br-0 bl-0 b--white" value={this.state.flexBasis} onChange={this.handleBasisChange} />
+            <label className={labelClassNames}>flexBasis:</label>
+            <input type="text" placeholder="flex-basis" className={inputClassNames} value={this.state.flexBasis} onChange={this.handleBasisChange} />
           </div>
           <div>
-            <label className="w4 pr1 courier white f6 tr dib">alignSelf:</label>
-            <select name="alignSelf" className="dib courier white bg-transparent w4 outline-0 input-reset br0 ba bw1 bt-0 br-0 bl-0 b--white" value={this.state.alignSelf} onChange={this.handleAlignSelfChange}>
+            <label className={labelClassNames}>alignSelf:</label>
+            <select name="alignSelf" className={inputClassNames} value={this.state.alignSelf} onChange={this.handleAlignSelfChange}>
               {this.props.itemProps.alignSelf.map(it => <option value={it} key={it}>{it}</option>)}
             </select>
           </div>
